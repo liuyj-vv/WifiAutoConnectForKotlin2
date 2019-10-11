37255a9ce47dcd4236f90775a625ef9c1bfd02d1
liuyj <353381808@qq.com>
2019-10-11 10:02:10 +0800

    manifest 中注册广播接收者，和服务

--- app/src/main/java/com/changhong/wifiautoconnectforkotlin2/BootCompletedReceiver.kt
+++ app/src/main/java/com/changhong/wifiautoconnectforkotlin2/BootCompletedReceiver.kt
@@ -11,7 +11,7 @@ class BootCompletedReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.e(TAG, Thread.currentThread().stackTrace[2].methodName + "[" + Thread.currentThread().stackTrace[2].lineNumber + "]")
         if (intent?.action == "android.intent.action.BOOT_COMPLETED") {
-            Toast.makeText(context, "收到开机广播 kotlin2 TAG", Toast.LENGTH_LONG).show()
+            Toast.makeText(context, "收到开机广播 kotlin2 $TAG", Toast.LENGTH_LONG).show()
             val i = Intent(context, MainService::class.java)
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             context?.startService(i)
